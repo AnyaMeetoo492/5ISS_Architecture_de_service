5e4505cce76fea24ae35c2508d23a072de8d720e
AnyaMeetoo492 <anyameetoo@gmail.com>
2026-01-14 13:34:27 +0100

    [ADD] config service

---
+++ Microservices/Configuration/src/test/java/fr/insa/ms/Configuration/ConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.ms.Configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
